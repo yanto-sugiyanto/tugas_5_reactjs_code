f09efb4c7c31b5c26f1d804152e4a05c42e4f386
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-09-30 21:06:57 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan'
+
+function App() {
+  return (
+    <div>
+    <MenuMakanan />
+
+    </div>
+  );
+}
+
+export default App;
